5926ac2308e25314db037f9af9c8df0d9d99828f
Robin Ellingsen <robin-elli@hotmail.com>
2024-03-19 09:27:15 +0100

    new logo files

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <router-link to="/" style="color: inherit; text-decoration: inherit;">
     <div style="display: flex; gap: 0.5rem; align-items: center;">
       <div class="header-part">
-        <img :src="require('@/assets/tfk_header_logo.svg')" style="height: 3rem"/>
+        <img :src="require('@/assets/header_logo.svg')" style="height: 3rem"/>
       </div>
       <!-- <div class="header-part typography heading-two">
         Masseutsendelse
